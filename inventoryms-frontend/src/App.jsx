362391b596b2008bf7298c5ed6e04a38dc6d6a02
SambhavSurthi <sambhavsurthi14@gmail.com>
2025-09-18 22:09:32 +0530

    added inital project

---
+++ inventoryms-frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-center text-5xl font-semibold underline  pt-10' >inventory management system </div>
+  )
+}
+
+export default App
